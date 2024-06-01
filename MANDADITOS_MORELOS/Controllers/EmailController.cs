a36a2fdeff326b6084a1d2700e6414c637e2f8ef
Sebas-ctrl <210505@utxicotepec.edu.mx>
2024-06-01 12:55:37 -0600

    [SMG] Primera versión de la API para el envío de correo electrónico

---
+++ MANDADITOS_MORELOS/Controllers/EmailController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MANDADITOS_MORELOS.Models;
+using System.Net.Mail;
+using System.Net;
+
+namespace MANDADITOS_MORELOS.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        // POST: api/Email
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Boolean>> PostEmail(String email)
+        {
+            var client = new SmtpClient("sandbox.smtp.mailtrap.io", 2525)
+            {
+                Credentials = new NetworkCredential("a78883aeeecc97", "56be617b37b205"),
+                EnableSsl = true
+            };
+            client.Send("shadowtic9@gmail.com", email, "Hola", "testbody");
+            if(client.Credentials != null)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ MANDADITOS_MORELOS/DbContextConfigurator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MANDADITOS_MORELOS
+{
+    public static class DbContextConfigurator
+    {
+        public static void ConfigureDbContext<TContext>(IServiceCollection services, IConfiguration configuration)
+            where TContext : DbContext
+        {
+            services.AddDbContext<TContext>(options =>
+            {
+                var connectionString = configuration.GetConnectionString("connection_to_mysql");
+                options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 36)));
+            });
+        }
+    }
+}
